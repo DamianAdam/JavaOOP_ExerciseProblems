aa089cfb476c7615c0023b74a11926365dfe539a
DamianAdam <62690738+DamianAdam@users.noreply.github.com>
2021-01-27 21:00:38 +0800

    Update StudentTest.java

--- 01. ObjectArrays/StudentTest.java
+++ 01. ObjectArrays/StudentTest.java
@@ -1,26 +1,35 @@
 public class StudentTest {
 	public static void main(String[] args) {
 		// 创建20个学生对象，并赋值属性。
-		Student[] schoolStudent = new Student[20];
+		int num = 20;
+		Student[] schoolStudent = makeStuArray(num);
+		
+		System.out.println("解题目一");
+		// 打印出3年级的学生信息
+		searchState(3, schoolStudent);
+		
+		System.out.println("解题目二");
+		// 按照学生成绩冒泡排序
+		schoolStudent = bubbleSortStudent(schoolStudent);
+		// 遍历学生信息
+		printArrays(schoolStudent);
+	}
+	
+	// 创建学生对象,并自动初始化属性
+	public static Student[] makeStuArray(int num) {
+		Student[] schoolStudent = new Student[num];
 		for (int i = 0; i < schoolStudent.length; i++) {
 			schoolStudent[i] = new Student();
 			schoolStudent[i].number = i + 1;
 			schoolStudent[i].state = (int)(Math.random() * 3 + 1);
 			schoolStudent[i].score = (int)(Math.random() * 101);
 		}
-		// 打印出3年级的学生信息
-		System.out.println("解题目一");
-		System.out.println("成绩\t学号\t年级");
-		for (int i = 0; i < schoolStudent.length; i++) {
-			if (schoolStudent[i].state == 3) {
-				System.out.println(schoolStudent[i].score + "\t" + schoolStudent[i].number + "\t" + schoolStudent[i].state);
-			}
-		}
-		System.out.println();
-		
-		// 按照学生成绩冒泡排序
-		System.out.println("解题目二");
-        for (int i = 0; i < schoolStudent.length - 1; i++) {
+		return schoolStudent;
+	}
+	
+	// 冒泡排序学生数组
+	public static Student[] bubbleSortStudent(Student[] schoolStudent) {
+		for (int i = 0; i < schoolStudent.length - 1; i++) {
             for (int j = 0; j < schoolStudent.length - 1 - i; j++) {
                 if (schoolStudent[j].score > schoolStudent[j + 1].score) {
                     Student temp = schoolStudent[j];
@@ -29,7 +38,22 @@ public class StudentTest {
                 }
             }
         }
-		// 遍历学生信息
+		return schoolStudent;
+	}
+	
+	// 查找对应年级的学生
+	public static void searchState(int numState, Student[] schoolStudent) {
+		System.out.println("成绩\t学号\t年级");
+		for (int i = 0; i < schoolStudent.length; i++) {
+			if (schoolStudent[i].state == numState) {
+				System.out.println(schoolStudent[i].score + "\t" + schoolStudent[i].number + "\t" + schoolStudent[i].state);
+			}
+		}
+		System.out.println();
+	}
+	
+	// 遍历schoolStudent数组
+	public static void printArrays(Student[] schoolStudent) {
 		System.out.println("成绩\t学号\t年级");
 		for (int i = 0; i < schoolStudent.length; i++) {
 			System.out.println(schoolStudent[i].score + "\t" + schoolStudent[i].number + "\t" + schoolStudent[i].state);
